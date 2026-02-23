96c1f04a145f7de0f6537ab8b02d1a2d541acbb0
law8cam <law8cam@gmail.com>
2026-02-23 22:30:25 +0000

    Jekyll Update

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    outDir: './docs', // This tells Astro: "Build into /docs instead of /dist"
+});
